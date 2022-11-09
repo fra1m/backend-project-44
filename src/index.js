78f6c62c909ac5019aa4b3511006be352ab01df7
vasili <frugas@list.ru>
2022-11-09 17:19:29 +0300

    8th step compliated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-export { result, NOD };
+import _ from 'lodash';
+
+export {
+  result, NOD, randomArray, randomRealAnswer,
+};
 
 const result = (realAnswer, answer, name) => {
   console.log(`Your answer: ${answer}`);
@@ -18,4 +22,23 @@ const NOD = (num1, num2) => {
     return minNum;
   }
   return NOD(nod, minNum);
+};
+
+const randomArray = () => {
+  const n = _.random(5, 10);
+  const x = _.random(1, 5);
+  const masNums = [];
+  masNums.push(x);
+  for (let i = 1; i < n; i++) {
+    masNums.push(masNums[i - 1] + x);
+  }
+
+  return masNums;
+};
+
+const randomRealAnswer = (mass) => {
+  const missedIndex = _.random(0, mass.length - 1);
+  const realAnswer = mass[missedIndex];
+
+  return String(realAnswer);
 };
--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,13 @@
 import readlineSync from 'readline-sync';
 import _ from 'lodash';
 import whatName from './cli.js';
-import { result, NOD } from './index.js';
+import {
+  result, NOD, randomArray, randomRealAnswer,
+} from './index.js';
 
-export { even, calculator, findGCD };
+export {
+  even, calculator, findGCD, progression,
+};
 
 const even = () => {
   const name = whatName();
@@ -44,6 +48,21 @@ const findGCD = () => {
     let realAnswer = NOD(num1, num2);
     realAnswer = String(realAnswer);
     const answer = readlineSync.question(`Qustion: ${num1} ${num2} `);
+    if (result(realAnswer, answer, name) === false) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+const progression = () => {
+  const name = whatName();
+  console.log('What number is missing in the progression?');
+  for (let i = 0; i < 3; i++) {
+    const mass = randomArray();
+    const realAnswer = randomRealAnswer(mass);
+    mass[mass.indexOf(Number(realAnswer))] = '..';
+    const answer = readlineSync.question(`Qustion: ${mass} `);
     if (result(realAnswer, answer, name) === false) {
       return;
     }
